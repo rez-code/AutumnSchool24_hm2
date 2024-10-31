8087c0c7b2689f80dec16a0e0017b5105833a6c2
rez <>
2024-10-31 22:32:50 +0300

    edited AnimationsViewController.swift

--- AutumnSchool24_hm2/AutumnSchool24_hm2/AnimationsViewController.swift
+++ AutumnSchool24_hm2/AutumnSchool24_hm2/AnimationsViewController.swift
@@ -1,22 +1,28 @@
 //
-//  ViewController.swift
+//  AnimationsViewController.swift
 //  AutumnSchool24_hm2
 //
 //  Created by rez on 28.10.2024.
 //
 
 import UIKit
+import Lottie
 
 final class AnimationsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 	
+	
+	// MARK: - Properties
+	private var selectedAnimationIndex: Int? = nil
+	
+	
 	// MARK: - Constants
-	private enum Constants {
+	private struct Constants: Codable {
 		// background
 		static let animateInterval: Double = 5.0
 		// headerTitle
 		static let headerTitleTopInset: CGFloat = 50
 		// animations table
-		static let animations = ["animation 1", "animation 2", "animation 3"]
+		static let animationsNames = ["animation 1", "animation 2", "animation 3"]
 		// speed
 		static let segmentItemsSpeed = ["0.5x", "1.0x", "2.0x"]
 		// color
@@ -25,9 +31,10 @@ final class AnimationsViewController: UIViewController, UITableViewDelegate, UIT
 		static let cornerRadiusButton: CGFloat = 10
 	}
 	
-	// MARK: - Private methods
 	
+	// MARK: - UI Elements
 	private let gradientLayer = CAGradientLayer()
+	private lazy var animationView: LottieAnimationView = LottieAnimationView()
 	
 	private lazy var headerTitle: UILabel = {
 		let label = UILabel()
@@ -44,6 +51,7 @@ final class AnimationsViewController: UIViewController, UITableViewDelegate, UIT
 		tableView.dataSource = self
 		tableView.backgroundColor = .clear
 		tableView.separatorColor = .label
+		tableView.isScrollEnabled = false
 		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
 		return tableView
 	}()
@@ -90,12 +98,85 @@ final class AnimationsViewController: UIViewController, UITableViewDelegate, UIT
 		return button
 	}()
 	
-	// MARK: - Actions
-	@objc private func runButtonTapped() {
+	private lazy var mainStackView: UIStackView = {
+		let stackView = UIStackView(arrangedSubviews: [
+			headerTitle,
+			tableAnimations,
+			speedTitle,
+			speedSegment,
+			colorTitle,
+			colorSegment,
+			runButton
+		])
+		stackView.axis = .vertical
+		stackView.alignment = .center
+		stackView.spacing = 50
+		return stackView
+	}()
+	
+	
+	// MARK: - View Lifecycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setupUI()
 		
 	}
 	
-	// MARK: - animateGradient
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		gradientLayer.frame = view.bounds
+	}
+	
+	
+	// MARK: - Actions
+	@objc private func runButtonTapped() {
+		guard let selectedIndex = selectedAnimationIndex else {
+			showAlert(message: "Анимация не выбрана!")
+			return
+		}
+		
+		let animationName = Constants.animationsNames[selectedIndex]
+		let speed: CGFloat = [0.5, 1.0, 2.0][speedSegment.selectedSegmentIndex]
+		let backgroundColor: UIColor = [.black, .gray, .white][colorSegment.selectedSegmentIndex]
+		
+		UIView.animate(withDuration: 0.3, animations: {
+			self.runButton.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+		}) { _ in
+			UIView.animate(withDuration: 0.3, animations: {
+				self.runButton.transform = .identity
+			}) { _ in
+				self.showAnimationView(animationName: animationName, speed: speed, backgroundColor: backgroundColor)
+			}
+		}
+	}
+	
+	
+	// MARK: - Helper Methods
+	private func showAlert(message: String) {
+		let alert = UIAlertController(title: "Ошибка", message: message, preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "ОК", style: .default, handler: nil))
+		present(alert, animated: true, completion: nil)
+	}
+	
+	private func showAnimationView(animationName: String, speed: CGFloat, backgroundColor: UIColor) {
+		let animationContainerView = UIView(frame: view.bounds)
+		animationContainerView.backgroundColor = backgroundColor
+		view.addSubview(animationContainerView)
+		
+		animationView = LottieAnimationView(name: animationName)
+		animationView.frame = animationContainerView.bounds
+		animationView.contentMode = .scaleAspectFit
+		animationView.animationSpeed = speed
+		animationContainerView.addSubview(animationView)
+		
+		animationView.play { [weak self] _ in
+			animationContainerView.removeFromSuperview()
+			self?.animationView.removeFromSuperview()
+		}
+	}
+	
+	
+	// MARK: - Animate Gradient
 	private func animateGradient(duration: CFTimeInterval = Constants.animateInterval, repeatCount: Float = .infinity) {
 		let colorChangeAnimation = CABasicAnimation(keyPath: "colors")
 		colorChangeAnimation.fromValue = [UIColor.blue.cgColor, UIColor.magenta.cgColor]
@@ -107,36 +188,42 @@ final class AnimationsViewController: UIViewController, UITableViewDelegate, UIT
 	}
 	
 	
-	// MARK: - numberOfRowsInSection
+	// MARK: - UITableViewDataSource
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return Constants.animations.count
+		return Constants.animationsNames.count
 	}
 	
-	// MARK: - heightForRowAt
-	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		return 43
-	}
-	
-	// MARK: - cellForRowAt
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-		cell.textLabel?.text = Constants.animations[indexPath.row]
+		cell.textLabel?.text = Constants.animationsNames[indexPath.row]
 		cell.textLabel?.font = UIFont.systemFont(ofSize: 22, weight: .regular)
 		cell.textLabel?.textColor = .label
 		cell.backgroundColor = .clear
 		return cell
 	}
 	
-	// MARK: - setupUi
-	private func setupUi() {
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 43
+	}
+	
+	
+	// MARK: - UITableViewDelegate
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		selectedAnimationIndex = indexPath.row
+	}
+	
+	
+	// MARK: - Setup UI
+	private func setupUI() {
 		setupGradient()
+		setupStackView()
 		setupHeader()
 		setupTableAnimationsUI()
 		setupSpeedControls()
 		setupColorControls()
 		setupRunButton()
 	}
-
+	
 	private func setupGradient() {
 		gradientLayer.colors = [UIColor.blue.cgColor, UIColor.magenta.cgColor]
 		gradientLayer.startPoint = CGPoint(x: 0, y: 0)
@@ -145,86 +232,55 @@ final class AnimationsViewController: UIViewController, UITableViewDelegate, UIT
 		view.layer.addSublayer(gradientLayer)
 		animateGradient()
 	}
-
-	private func setupHeader() {
-		headerTitle.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(headerTitle)
+	
+	private func setupStackView() {
+		mainStackView.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(mainStackView)
 		NSLayoutConstraint.activate([
-			headerTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			headerTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Constants.headerTitleTopInset)
+			mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+			mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+			mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
 		])
 	}
-
+	
+	private func setupHeader() {
+		headerTitle.textAlignment = .center
+		mainStackView.addArrangedSubview(headerTitle)
+	}
+	
 	private func setupTableAnimationsUI() {
 		tableAnimations.backgroundColor = .clear
-		tableAnimations.separatorColor = .black
-		tableAnimations.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(tableAnimations)
-		NSLayoutConstraint.activate([
-			tableAnimations.topAnchor.constraint(equalTo: headerTitle.bottomAnchor, constant: 56),
-			tableAnimations.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			tableAnimations.widthAnchor.constraint(equalToConstant: 300),
-			tableAnimations.heightAnchor.constraint(equalToConstant: 133)
-		])
+		tableAnimations.separatorColor = .label
+		tableAnimations.heightAnchor.constraint(equalToConstant: 133).isActive = true
+		tableAnimations.widthAnchor.constraint(equalTo: mainStackView.widthAnchor).isActive = true
+		mainStackView.addArrangedSubview(tableAnimations)
 	}
 	
 	private func setupSpeedControls() {
 		speedTitle.textAlignment = .center
-		speedTitle.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(speedTitle)
-		NSLayoutConstraint.activate([
-			speedTitle.topAnchor.constraint(equalTo: tableAnimations.bottomAnchor, constant: 56),
-			speedTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-		])
+		mainStackView.addArrangedSubview(speedTitle)
 		
-		speedSegment.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(speedSegment)
-		NSLayoutConstraint.activate([
-			speedSegment.topAnchor.constraint(equalTo: speedTitle.bottomAnchor, constant: 24),
-			speedSegment.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			speedSegment.widthAnchor.constraint(equalToConstant: 300)
-		])
+		speedSegment.widthAnchor.constraint(equalTo: mainStackView.widthAnchor).isActive = true
+		mainStackView.addArrangedSubview(speedSegment)
 	}
 	
 	private func setupColorControls() {
 		colorTitle.textAlignment = .center
-		colorTitle.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(colorTitle)
-		NSLayoutConstraint.activate([
-			colorTitle.topAnchor.constraint(equalTo: speedSegment.bottomAnchor, constant: 56),
-			colorTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-		])
+		mainStackView.addArrangedSubview(colorTitle)
 		
-		colorSegment.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(colorSegment)
-		NSLayoutConstraint.activate([
-			colorSegment.topAnchor.constraint(equalTo: colorTitle.bottomAnchor, constant: 24),
-			colorSegment.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			colorSegment.widthAnchor.constraint(equalToConstant: 300)
-		])
+		colorSegment.widthAnchor.constraint(equalTo: mainStackView.widthAnchor).isActive = true
+		mainStackView.addArrangedSubview(colorSegment)
 	}
 	
 	private func setupRunButton() {
-		runButton.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(runButton)
-		NSLayoutConstraint.activate([
-			runButton.topAnchor.constraint(equalTo: colorSegment.bottomAnchor, constant: 56),
-			runButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			runButton.widthAnchor.constraint(equalToConstant: 195),
-			runButton.heightAnchor.constraint(equalToConstant: 49)
-		])
-	}
-	
-	// MARK: - viewDidLoad
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		setupUi()
-		
-	}
-	
-	// MARK: - viewDidLayoutSubviews
-	override func viewDidLayoutSubviews() {
-		super.viewDidLayoutSubviews()
-		gradientLayer.frame = view.bounds
+		runButton.widthAnchor.constraint(equalToConstant: 195).isActive = true
+		runButton.heightAnchor.constraint(equalToConstant: 49).isActive = true
+		mainStackView.addArrangedSubview(runButton)
 	}
 }
+
+
+//@available(iOS 17.0, *)
+//#Preview {
+//	AnimationsViewController()
+//}
